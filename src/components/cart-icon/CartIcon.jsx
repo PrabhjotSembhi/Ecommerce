26f713f508a04d17bd9563523bb0e1f5c76f568e
PrabhjotSembhi <prabh.s2508@gmail.com>
2023-02-17 10:53:41 +0530

    working on cart funtionality, Icon added in navba

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import { ReactComponent as ShopingIcon } from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 const CartIcon = () => {
+const [cartOpened, setCartOpened] = useState(false)
+
+
+
   return (
     <div className='cart-icon-container'>
-        <ShopingIcon className='shopping-icon'/>
+        <ShopingIcon className='shopping-icon' onClick={() => {setCartOpened(!cartOpened)}}/>
         <span className='item-count'>10</span>
+     
     </div>
   )
 }
